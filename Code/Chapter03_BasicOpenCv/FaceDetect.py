4fc3d5b14b4fefe3612e7865647d56284c45abba
Quyen Ngo <qsbk07@gmail.com>
2020-03-18 23:24:19 +0700

    Update Lecture

--- Code/Chapter03_BasicOpenCv/FaceDetect.py
+++ Code/Chapter03_BasicOpenCv/FaceDetect.py
@@ -1,9 +1,10 @@
-# Run script: python3 FaceDetect.py --image Images/TwoFaces --detectModel HaarCascadeFrontalfaceDefault.xml
+# Chay chuong trinh: python3 FaceDetect.py --image Images/TwoFaces --detectModel HaarCascadeFrontalfaceDefault.xml
 import argparse
 import cv2
 
 
-# create arg parser and parse the arg
+# Tao doi tuong argP dung de xay dung list argument can truyen vao cho chuong trinh
+# Sau do parse argment ra 1 dictinary de su dung trong chuong trinh
 argP = argparse.ArgumentParser()
 argP.add_argument("-i", "--image", required=True, 
     help="path to input image")
@@ -11,16 +12,18 @@ argP.add_argument("-d", "--detectModel", required=True,
     help="path to Haar cascade .xml face detector model")    
 args = vars(argP.parse_args())
 
-# load img and convert to gray
+# Tai anh len va chuyen sang gray scale
 image = cv2.imread(args["image"])
 grayImg = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# load face detector and detect face
+# Tai face detector model len va tien hanh detect face
 detector = cv2.CascadeClassifier(args["detectModel"])
-rectsBox = detector.detectMultiScale(grayImg, scaleFactor=1.05, minNeighbors=9, minSize=(40,40), flags=cv2.CASCADE_SCALE_IMAGE)
+rectsBox = detector.detectMultiScale(grayImg, scaleFactor=1.05, minNeighbors=9, 
+            minSize=(40,40), flags=cv2.CASCADE_SCALE_IMAGE)
 print("[INFO] detected {} faces".format(len(rectsBox)))
 
-#loop over rect bounding box(x,y,w,h) and draw blue rect with thick=3
+#loop qua tat ca cac hinh chu nhat bao(x,y,w,h) vua tim duoc
+#  va ve blue rect voi do day net ve la 3
 for (x,y,w,h) in rectsBox:
     cv2.rectangle(image, (x,y), (x+w, y+h), (255,0,0), 3)
 
